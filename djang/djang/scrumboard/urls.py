6a21de3bb7d1f7e3785b82949bccbb023c134cbd
Joshua Efe <joshuaefe@TsmnbxRemix.local>
2017-09-14 16:04:34 -0400

    Update

--- djang/djang/scrumboard/urls.py
+++ djang/djang/scrumboard/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from .api import ListApi, CardApi
 
@@ -13,5 +14,6 @@ from .api import ListApi, CardApi
 
 urlpatterns = [
     url(r'^list$', ListApi.as_view()),
-    url(r'^cards$', CardApi.as_view())
+    url(r'^cards$', CardApi.as_view()),
+    url(r'^home', TemplateView.as_view(template_name = "scrumboard/home.html")),
 ]
